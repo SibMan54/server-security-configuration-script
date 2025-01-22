dcbdcc62238fe7d5bbcb06c447c6805a6245151d
SibMan54 <ilya89vet@gmail.com>
2025-01-22 18:32:36 +0700

    Add backup config files

--- auto_updates_enable.sh
+++ auto_updates_enable.sh
@@ -1,5 +1,17 @@
-
 #!/bin/bash
+
+# Функция для создания резервной копии файла
+backup_file() {
+    local file="$1"
+    local backup="$1.bak"
+
+    if [ ! -f "$backup" ]; then
+        echo "Создаю резервную копию файла $file..."
+        cp "$file" "$backup"
+    else
+        echo "Резервная копия файла $file уже существует."
+    fi
+}
 
 # Проверяем наличие пакета unattended-upgrades
 if ! which unattended-upgrades > /dev/null 2>&1; then
@@ -15,23 +27,25 @@ dpkg-reconfigure --priority=low unattended-upgrades
 
 # Редактируем конфигурационный файл
 CONF_FILE="/etc/apt/apt.conf.d/50unattended-upgrades"
-echo "Редактируем файл конфигурации $CONF_FILE"
-sed -i 's|^\s*"\${distro_id}:\${distro_codename}";|//      "${distro_id}:${distro_codename}";|g' $CONF_FILE
-sed -i 's|//Unattended-Upgrade::Automatic-Reboot "false";|Unattended-Upgrade::Automatic-Reboot "true";|g' $CONF_FILE
+backup_file "$CONF_FILE"
+echo "Редактируем файл конфигурации $CONF_FILE..."
+sed -i 's|^\s*"\${distro_id}:\${distro_codename}";|//      "${distro_id}:${distro_codename}";|g' "$CONF_FILE"
+sed -i 's|//Unattended-Upgrade::Automatic-Reboot "false";|Unattended-Upgrade::Automatic-Reboot "true";|g' "$CONF_FILE"
 
 # Проверяем файл 20auto-upgrades
 AUTO_UPGRADES_FILE="/etc/apt/apt.conf.d/20auto-upgrades"
+backup_file "$AUTO_UPGRADES_FILE"
 echo "Включаем ежедневные обновления в $AUTO_UPGRADES_FILE..."
-if ! grep -q "APT::Periodic::Update-Package-Lists" $AUTO_UPGRADES_FILE; then
-    echo 'APT::Periodic::Update-Package-Lists "1";' >> $AUTO_UPGRADES_FILE
+if ! grep -q "APT::Periodic::Update-Package-Lists" "$AUTO_UPGRADES_FILE"; then
+    echo 'APT::Periodic::Update-Package-Lists "1";' >> "$AUTO_UPGRADES_FILE"
 else
-    sed -i 's/^APT::Periodic::Update-Package-Lists.*/APT::Periodic::Update-Package-Lists "1";/' $AUTO_UPGRADES_FILE
+    sed -i 's/^APT::Periodic::Update-Package-Lists.*/APT::Periodic::Update-Package-Lists "1";/' "$AUTO_UPGRADES_FILE"
 fi
 
-if ! grep -q "APT::Periodic::Unattended-Upgrade" $AUTO_UPGRADES_FILE; then
-    echo 'APT::Periodic::Unattended-Upgrade "1";' >> $AUTO_UPGRADES_FILE
+if ! grep -q "APT::Periodic::Unattended-Upgrade" "$AUTO_UPGRADES_FILE"; then
+    echo 'APT::Periodic::Unattended-Upgrade "1";' >> "$AUTO_UPGRADES_FILE"
 else
-    sed -i 's/^APT::Periodic::Unattended-Upgrade.*/APT::Periodic::Unattended-Upgrade "1";/' $AUTO_UPGRADES_FILE
+    sed -i 's/^APT::Periodic::Unattended-Upgrade.*/APT::Periodic::Unattended-Upgrade "1";/' "$AUTO_UPGRADES_FILE"
 fi
 
 # Запуск и включение службы unattended-upgrades
