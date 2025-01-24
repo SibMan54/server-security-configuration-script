65fb0a0a6cbc7a6a4b43d61c93b813c3377d145c
SibMan54 <ilya89vet@gmail.com>
2025-01-24 20:52:37 +0700

    Fix color and mesange

--- security_configuration_script.sh
+++ security_configuration_script.sh
@@ -110,8 +110,8 @@ fi
 # Конфигурация SSH
 
 # Вычисляем номер текущего порта SSH
-# SSH_PORT=$(grep -i "Port " /etc/ssh/sshd_config | awk '{print $2}')
-SSH_PORT=$(awk '$1 == "Port" {print $2; exit}' /etc/ssh/sshd_config)
+SSH_PORT=$(grep -i "Port " /etc/ssh/sshd_config | awk '{print $2}')
+# SSH_PORT=$(awk '$1 == "Port" {print $2; exit}' /etc/ssh/sshd_config)
 info "Текущий порт SSH: $SSH_PORT"
 # Путь к файлу конфигурации SSH
 SSH_CONFIG="/etc/ssh/sshd_config"
@@ -156,7 +156,7 @@ else
 fi
 
 # Проверка подключения
-info "Попробуйте подключиться по SSH с новым пользователем $username и портом $NEW_PORT..."
+info "Попробуйте подключиться по SSH с новым пользователем $username и портом $SSH_PORT или $NEW_PORT ..."
 read -p "$(echo -e "${YELLOW}Получилось ли подключиться? (y/n): ${NC}")" answer
 if [[ "$answer" == "y" ]]; then
     success "SSH-соединение успешно установлено."
@@ -220,31 +220,31 @@ fi
 
 echo ""
 echo "==========================================================="
-success "1. Создан новый пользователь $username"
-success "2. Публичный SSH ключ успешно добавлен в: $USER_KEY_PATH"
-success "3. Конфигурация SSH успешно изменена, порт изменен на $NEW_PORT. Используйте его при следующем подключении к серверу."
+echo -e "1. ${GREEN}Создан новый пользователь $username.${NC}"
+echo -e "2. ${GREEN}Публичный SSH ключ успешно добавлен в: $USER_KEY_PATH.${NC}"
+echo -e "3. ${GREEN}Конфигурация SSH успешно изменена, порт изменен на $NEW_PORT. Используйте его при следующем подключении к серверу.${NC}"
 counter=3
 ufw_status=$(sudo ufw status | grep -o "Status: active")
 if [ "$ufw_status" == "Status: active" ]; then
     counter=$((counter + 1))
-    success "$counter. Firewall активирован, порт $NEW_PORT добавляем в разрешенные."
+    echo -e "$counter. ${GREEN}Firewall активирован, порт $NEW_PORT добавляем в разрешенные.${NC}"
 fi
 CONF_BAK_FILE="/etc/apt/apt.conf.d/50unattended-upgrades.bak"
 if systemctl is-active --quiet unattended-upgrades; then
     if [[ -f "$CONF_BAK_FILE" ]]; then
         counter=$((counter + 1))
-        success "$counter. Автоматическая проверка и установка обновлений безопасности включена и настроена."
+        echo -e "$counter. ${GREEN}Автоматическая проверка и установка обновлений безопасности включена и настроена.${NC}"
     else
     counter=$((counter + 1))
-    warning "$counter. Автоматическая проверка и установка обновлений включена, но изменения для безопастности обновлений не были внесены."
+    echo -e "$counter. ${BLUE}Автоматическая проверка и установка обновлений включена, но изменения для безопастности обновлений не были внесены.${NC}"
     fi
 fi
 if command -v x-ui &> /dev/null; then
     counter=$((counter + 1))
-    success "$counter. Панель 3X-UI установлена."
+    echo -e "$counter. ${GREEN}Панель 3X-UI установлена.${NC}"
 fi
-success ">> Настройка завершена, сервер теперь в безопастности!"
-warning "Чтобы изменения вступили в силу, необходимо:"
+echo -e "${GREEN}✅ Настройка завершена, сервер теперь в безопастности!${NC}"
+echo -e "${YELLOW}⚠️ Чтобы изменения вступили в силу, необходимо:${NC}"
 echo -e "${CYAN}- Перезагрузить SSH-сервис командой ->${NC} systemctl restart ssh"
 echo -e "${CYAN}- Или полностью перезагрузить сервер командой ->${NC} reboot"
 echo "==========================================================="
